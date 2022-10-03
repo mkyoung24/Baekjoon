97ddb3d61c3c6e6ebd480c679ed8e1e88ffefc3e
mkyoung24 <103173521+mkyoung24@users.noreply.github.com>
2022-10-04 00:18:46 +0900

    Create 문자열 반복_2675.java

---
+++ 문자열 반복_2675.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Alg2675 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			int num = scan.nextInt();
+			String str = scan.next();
+			
+			for(int j=0; j<str.length(); j++) {
+				for(int k=0; k<num; k++) {
+					System.out.print(str.charAt(j));
+				}
+			}
+			System.out.println();
+			
+			
+		}
+		
+		
+
+	}
+
+}
