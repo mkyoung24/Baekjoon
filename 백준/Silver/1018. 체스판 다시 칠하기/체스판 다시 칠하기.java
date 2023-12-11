f722c2be3d2574a2ac7ce5b251785e1d5a5a46af
mkyoung24 <103173521+mkyoung24@users.noreply.github.com>
2023-12-11 15:41:58 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 84 ms, Memory: 11664 KB -BaekjoonHub

---
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static boolean[][] arr;
+	
+	static int n,m;
+	
+	static int min = 64;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		arr = new boolean[n][m];
+		
+		for (int i = 0; i < n; i++) {
+			String str = br.readLine();
+			for (int j = 0; j < m; j++) {
+				if(str.charAt(j) == 'W') {
+					arr[i][j] = true;
+				} else {
+					arr[i][j] = false;
+				}
+			}
+		}
+		
+		for (int i = 0; i < n - 7; i++) {
+			for (int j = 0; j < m - 7; j++) {
+				check(i, j);
+			}
+		}
+		
+		System.out.println(min);
+	}
+	
+	public static void check(int y, int x) {
+		int count = 0;
+		
+		boolean first = arr[y][x];
+		
+		for (int i = y; i < y + 8; i++) {
+			for (int j = x; j < x + 8; j++) {
+				if (arr[i][j] != first) {
+					count++;
+				}
+				
+				first = (!first);
+			}
+			
+			first = (!first);
+		}
+		
+		count = Math.min(count, 64 - count);
+		
+		min = Math.min(min, count);
+		
+	}
+}
